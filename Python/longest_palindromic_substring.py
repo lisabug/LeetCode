497c04420cb3f6e6c3e519aa8de50aedf2af2a38
lisabug <luyuanqin09@gmail.com>
2015-05-02 03:23:55 +0900

    longest palindromic substring

---
+++ Python/longest_palindromic_substring.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @author: Yuanqin Lu
+
+def longestPalindrome(s):
+    # construct special structure to find solution easily
+    ss = "$#" + reduce(plus_symbol, s) + "#"
+    m = id = 1
+    p = [1] * len(ss)
+
+    # calculate the longest palindrome string: index of center and the length
+    for (i, c) in enumerate(ss):
+        if i < m:
+            p[i] = min(m-i, p[2*id-i])
+            print p
+        while i+p[i] < len(ss) and i - p[i] >= 0 and ss[i+p[i]] == ss[i-p[i]]:
+            p[i] += 1
+        if p[i] > p[id]:
+            id = i
+            m = i + p[i]
+
+    # find longest palindrome substring
+    longest_center_index = p.index(max(p))
+    tmp_longest = ss[(longest_center_index-p[longest_center_index]+1):(longest_center_index+p[longest_center_index]-1)]
+    longest = ""
+    for x in tmp_longest:
+        if x != "#" and x != "$":
+            longest += x
+
+    return longest
+
+
+def plus_symbol(p, q):
+    return p + "#" + q
+
+print longestPalindrome("a")
